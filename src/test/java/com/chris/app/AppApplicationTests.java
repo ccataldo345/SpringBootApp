06c155058390233f3d9680fab92c7a0d63062e06
Christian Cataldo <epost345@gmail.com>
2020-04-24 01:31:59 +0300

    Add Spring Boot start files

---
+++ src/test/java/com/chris/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
