04a2de1c7ca27c0fc54605cc5b62675fdca8bb11
Christian Cataldo <epost345@gmail.com>
2020-04-29 14:57:29 +0300

    Add Lombok to Book entity

--- src/main/java/com/chris/app/entity/Book.java
+++ src/main/java/com/chris/app/entity/Book.java
@@ -1,8 +1,14 @@
 package com.chris.app.entity;
 
-import javax.persistence.*;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
+@Data
 public class Book {
 
     @Id
@@ -13,42 +19,6 @@ public class Book {
     private String author;
     private String isbn;
 
-    public Book() {
-    }
-
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
 }
 
 
